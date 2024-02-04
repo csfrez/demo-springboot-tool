2fb3c3a8aad798a3d1f91b85616075a272ab4af6
csfrez <csfrez@163.com>
2024-02-04 14:05:01 +0800

    初始化

---
+++ src/test/java/com/csfrez/tool/DemoSpringbootToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csfrez.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
