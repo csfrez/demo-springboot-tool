1d89dd6dd282120d3f9b710d462f321315285ee0
Yangzhi <yangzhi@ddjf.com.cn>
2024-02-04 14:05:01 +0800

    初始化

---
+++ src/main/java/com/csfrez/tool/DemoSpringbootToolApplication.java
@@ -0,0 +1,13 @@
+package com.csfrez.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootToolApplication.class, args);
+    }
+
+}
