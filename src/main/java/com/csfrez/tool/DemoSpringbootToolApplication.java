da95ac7fc0e16bb8c6636599ec5995535d6a9edc
Yangzhi <yangzhi@ddjf.com.cn>
2024-04-11 15:23:29 +0800

    响应数据封装统一封装

--- src/main/java/com/csfrez/tool/DemoSpringbootToolApplication.java
+++ src/main/java/com/csfrez/tool/DemoSpringbootToolApplication.java
@@ -1,9 +1,11 @@
 package com.csfrez.tool;
 
+import com.feiniaojin.gracefulresponse.EnableGracefulResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableGracefulResponse
 public class DemoSpringbootToolApplication {
 
     public static void main(String[] args) {
